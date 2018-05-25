ddc03180d7339c9ff3105107b0cebe478a275795
Jeremy Robst <jpro@bas.ac.uk>
2018-05-25 23:32:28 +0100

    Fix debugging

--- perl/Eventlog.pm
+++ perl/Eventlog.pm
@@ -203,7 +203,7 @@ sub get_streams {
 		$eventlog->{data}->attach($_);
 
 		print STDERR "Stream [$_]\n";
-		push(@data_streams, { stream => $eventlog->{data}->name() }; #, vars => $eventlog->{data}->vars() });
+		push(@data_streams, { stream => $eventlog->{data}->name() }); #, vars => $eventlog->{data}->vars() });
 
 		$eventlog->{data}->detach();
 	}
